3d8368f1ff1b2f44d5cdfefd2138fc1ce0578f91
Florent S <sandri822@hotmail.fr>
2023-08-10 12:56:16 +0200

    🛠️ Create Review model for Shift Heroes feedback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_084552) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_10_105406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_084552) do
     t.datetime "updated_at", null: false
     t.index ["shift_id"], name: "index_reservations_on_shift_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "stars"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shifts", force: :cascade do |t|
