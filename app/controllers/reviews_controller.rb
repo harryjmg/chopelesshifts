a6519cb9b9e01765630efb6c9deb083a36556b7d
Florent S <sandri822@hotmail.fr>
2023-08-11 10:59:09 +0200

    🔧 Center modal, add data attributes for modal

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,9 @@ class ReviewsController < ApplicationController
   def create_review
     @review = current_user.reviews.build(review_params)
     if @review.save
-      redirect_to videos_path, notice: 'Merci pour votre avis!'
+      redirect_to root_path, notice: 'Merci pour votre avis!'
     else
-      # TODO: handle error
-      puts "Failled to save review"
-      puts @review.errors.full_messages
+      flash[:alert] = 'Il y a eu un problème lors de la sauvegarde de votre avis'
     end
   end
 
