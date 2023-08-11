f5cbc4efb10eec2207ba8b8403075043871b6c81
Florent S <sandri822@hotmail.fr>
2023-08-11 16:09:27 +0200

    🧪 Add Review model validations and tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
 
+  validates :user, presence: true
+  validates :user_id, uniqueness: true
   validates :stars, presence: true, inclusion: { in: 1..5 }
   validates :comment, presence: true
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,30 @@
-require "test_helper"
+require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'review should belong to a user' do
+    review = Review.new(stars: 3, comment: 'Great!')
+    assert_not review.valid?
+    assert_includes review.errors.full_messages, 'User doit exister'
+  end
+
+  test 'review should have stars between 1 and 5' do
+    review = reviews(:one)
+    review.stars = 6
+    assert_not review.valid?
+    assert_includes review.errors.full_messages, "Stars n'est pas inclus(e) dans la liste"
+  end
+
+  test 'user should have only one review' do
+    user = users(:one)
+    first_review = Review.create(user: user, stars: 4, comment: 'Good!')
+    duplicate_review = Review.new(user: user, stars: 3, comment: 'Okay!')
+    assert_not duplicate_review.valid?
+    assert_includes duplicate_review.errors.full_messages, 'User est déjà utilisé(e)'
+  end
+
+  test 'user can update their review' do
+    review = reviews(:one)
+    review.comment = 'Updated comment'
+    assert review.save
+  end
 end
