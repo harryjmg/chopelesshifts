ab975ac3a1c4bef5781976b688c9c926057cf7c7
Florent S <sandri822@hotmail.fr>
2023-08-14 09:19:44 +0200

    🚀 Revolve PR confetti_video_unlock#2

--- test/integration/layout_integration_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LayoutIntegrationTest < ActionDispatch::IntegrationTest
-  test "should include confetti script in layout" do
-    get root_path
-
-    assert_select "script[src='https://cdn.jsdelivr.net/npm/canvas-confetti@1.6.0/dist/confetti.browser.min.js']", 1
-  end
-end
