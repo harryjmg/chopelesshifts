105313a2579648960afe39f218abb519d2f46060
harryjmg <harry.gueguen@gmail.com>
2023-09-25 13:43:02 +0200

    Fix too much reminders for last level

--- app/jobs/send_advices_to_user_job.rb
+++ app/jobs/send_advices_to_user_job.rb
@@ -14,16 +14,16 @@ class SendAdvicesToUserJob < ApplicationJob
 
       if last_known_level == 6
         return if user.achievements.where(key: "apply_for_coding_accelerator").any?
-        SendAdvicesToUserJob.set(wait: 7.days).perform_later(user_id, last_known_level, reminder_count)
         return if reminder_count > 2
-      end
-
-      if reminder_count <= 4
-        SendAdvicesToUserJob.set(wait: 1.day).perform_later(user_id, last_known_level, reminder_count)
-      elsif reminder_count <= 6
         SendAdvicesToUserJob.set(wait: 7.days).perform_later(user_id, last_known_level, reminder_count)
-      elsif reminder_count <= 8
-        SendAdvicesToUserJob.set(wait: 14.days).perform_later(user_id, last_known_level, reminder_count)
+      else
+        if reminder_count <= 4
+          SendAdvicesToUserJob.set(wait: 1.day).perform_later(user_id, last_known_level, reminder_count)
+        elsif reminder_count <= 6
+          SendAdvicesToUserJob.set(wait: 7.days).perform_later(user_id, last_known_level, reminder_count)
+        elsif reminder_count <= 8
+          SendAdvicesToUserJob.set(wait: 14.days).perform_later(user_id, last_known_level, reminder_count)
+        end
       end
     end
   end
