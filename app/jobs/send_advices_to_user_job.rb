d88b7d5207423508d77c13d11a789d8df0615b4c
scosy <amine.bentaleb064@gmail.com>
2024-06-06 20:03:12 +0200

    Commenting app/jobs/send_advices_to_user_job.rb

--- app/jobs/send_advices_to_user_job.rb
+++ app/jobs/send_advices_to_user_job.rb
@@ -2,29 +2,29 @@ class SendAdvicesToUserJob < ApplicationJob
   queue_as :default
 
   def perform(user_id, last_known_level, reminder_count = 0)
-    user = User.find(user_id)
+    # user = User.find(user_id)
 
-    return if user.subscribed_to_advices == false
+    # return if user.subscribed_to_advices == false
 
-    # Si son niveau n'a pas changé
-    if last_known_level == user.current_level
-      SendAdvicesToUserService.new(user_id).send_advice(last_known_level, reminder_count)
+    # # Si son niveau n'a pas changé
+    # if last_known_level == user.current_level
+    #   SendAdvicesToUserService.new(user_id).send_advice(last_known_level, reminder_count)
     
-      reminder_count += 1
+    #   reminder_count += 1
 
-      if last_known_level == 6
-        return if user.achievements.where(key: "apply_for_coding_accelerator").any?
-        return if reminder_count > 2
-        SendAdvicesToUserJob.set(wait: 7.days).perform_later(user_id, last_known_level, reminder_count)
-      else
-        if reminder_count <= 4
-          SendAdvicesToUserJob.set(wait: 1.day).perform_later(user_id, last_known_level, reminder_count)
-        elsif reminder_count <= 6
-          SendAdvicesToUserJob.set(wait: 7.days).perform_later(user_id, last_known_level, reminder_count)
-        elsif reminder_count <= 8
-          SendAdvicesToUserJob.set(wait: 14.days).perform_later(user_id, last_known_level, reminder_count)
-        end
-      end
-    end
+    #   if last_known_level == 6
+    #     return if user.achievements.where(key: "apply_for_coding_accelerator").any?
+    #     return if reminder_count > 2
+    #     SendAdvicesToUserJob.set(wait: 7.days).perform_later(user_id, last_known_level, reminder_count)
+    #   else
+    #     if reminder_count <= 4
+    #       SendAdvicesToUserJob.set(wait: 1.day).perform_later(user_id, last_known_level, reminder_count)
+    #     elsif reminder_count <= 6
+    #       SendAdvicesToUserJob.set(wait: 7.days).perform_later(user_id, last_known_level, reminder_count)
+    #     elsif reminder_count <= 8
+    #       SendAdvicesToUserJob.set(wait: 14.days).perform_later(user_id, last_known_level, reminder_count)
+    #     end
+      # end
+    # end
   end
 end
